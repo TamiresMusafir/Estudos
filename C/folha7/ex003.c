313f3eba036f9d0f41eed8d6dfb732deafca52bd
musafir <tamiresbarbosarj@gmail.com>
2025-11-13 16:50:42 -0300

    Ex003.c adicionado

--- C/folha7/ex003.c
+++ C/folha7/ex003.c
@@ -1,2 +1,49 @@
 #include <stdio.h>
+#define TOT 11
 
+struct depto{
+    int soma, qtd
+}typedef struct depto Tdepto;
+
+void inicializa(Tdepto v[]){
+    for(int i = 0; i < TOT; i++)
+        v[i].soma = 0;
+        v.qtd = 0;
+}
+
+void exibe(Tfunc v[]){
+    float media;
+
+    for(int i = 0; i < TOT; i++){
+        if(v[i]. qtd != 0)
+            media = (float)v[i].soma / v[i].qtd;
+        else
+            media = 0;
+        printf("\nDepartamento %d: %.2f", i+10, media);
+    }
+}
+
+int main(){
+    Tdepto v[TOT];
+    int cont, matr, dep, idade, qtd;
+    char sexo;
+
+    inicializa(v);
+
+    printf("Digite quantidade funcionarios: ");
+    scanf("%d", &qtd);
+
+    for(cont = 0; cont < qtd; cont++){
+        printf("Digite matrícula, departamento, idade e sexo: ");
+        scanf("%d%d%d %c", &matr, &dep, &idade, &sexo);
+
+        if(sexo == 'F'){
+            v[dep - 10].soma += idade;
+            v[dep - 10].qtd++;
+        }
+    }
+
+    exibe(v);
+
+    return 0;
+}
