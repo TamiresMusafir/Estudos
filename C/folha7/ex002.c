0647fc2d3eee764ff44fafdde04ef77f0dc9a5de
musafir <tamiresbarbosarj@gmail.com>
2025-11-13 16:11:06 -0300

    Ex002.c atualizado

--- C/folha7/ex002.c
+++ C/folha7/ex002.c
@@ -8,13 +8,16 @@ struct cliente{
 typedef struct cliente Tcliente;
 
 int leitura(Tcliente clientes[], float *media){
+    int conta;
     float soma = 0;
     int i = 0;
 
     printf("Digite o número da conta (digite 0 para terminar): \n");
-    scanf("%d", &clientes[i].conta);
+    scanf("%d", &conta); // varável normal para não guardar o 0;
 
-    while(clientes[i].conta != 0){
+    while(conta != 0 && i < MAX){
+
+        v[i].conta = conta;
         printf("\nDigite o saldo: ");
         scanf("%f", &clientes[i].saldo);
 
@@ -22,7 +25,7 @@ int leitura(Tcliente clientes[], float *media){
         i++;
 
         printf("Digite o número da conta (digite 0 para terminar): \n");
-        scanf("%d", &clientes[i].conta);
+        scanf("%d", &conta);
     }
 
     *media = soma / i;
@@ -37,7 +40,7 @@ int main(){
 
     qtd = leitura(clientes, &media);
 
-     printf("Média do saldo: %.2f\n", media);
+    printf("Média do saldo: %.2f\n", media);
 
     for(int i = 0; i < qtd; i++){
         printf("Cliente %d\n", i + 1);
