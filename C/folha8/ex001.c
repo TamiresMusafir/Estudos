5149943c7a0a607afc24851c50846f9703e68402
musafir <tamiresbarbosarj@gmail.com>
2025-11-30 11:29:30 -0300

    Folha 8 adicionada

---
+++ C/folha8/ex001.c
@@ -0,0 +1,24 @@
+//Faça uma função que receba como parâmetro uma matriz 6 x 12 de inteiros, já preenchida, contendo a quantidade mensal vendida do carro Gol nos anos 2010 a 2015 e um vetor contendo o preço do carro nos anos de 2010 a 2015. A função deverá exibir o valor total das vendas do Gol em cada ano (2010 a 2015).
+
+#include <stdio.h>
+
+/*void exibe(int qtdmensal[6][12], int preco[6]){
+     soma;
+    int i;
+    int j;
+
+    i = 0;  
+    while (i < 6)
+    {
+        j = 0;
+        while(j < 12)
+        {
+          soma += qtdmensal[i][j];
+          j++;
+        }int
+        i++;
+        float total = soma * preco[6];
+    }
+}*/
+
+void exibe(int qtdmensal[6][12], int preco[6])
