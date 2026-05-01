cc165fd669aa7530814b4588f18dd420bfea53fd
musafir <tamiresbarbosarj@gmail.com>
2026-05-01 17:29:43 -0300

    novo estudo adicionado

---
+++ java/br.com.lojavirtual/main/carrinho.java
@@ -0,0 +1,11 @@
+package br.com.lojavirtual.main;
+import br.com.lojavirtual.informatica.*;
+
+public class carrinho {
+    public static void main(String[] args){
+        Teclado x = new Teclado();
+        Teclado x2 = new Teclado();
+        br.com.lojavirtual.musica.teclado x3 = new br.com.lojavirtual.musica.Teclado();
+        moue m = new Mouse();
+    }
+}
---
+++ java/br.com.lojavirtual/musica/Teclado.java
@@ -0,0 +1,18 @@
+package br.com.lojavirtual.musica;
+
+public class Teclado {
+    private int numeroSons;
+    private String cor;
+
+    public void setusb(boolean usb){
+        
+    }
+
+    public String getCor(boolean usb){
+        return cor;
+    }
+
+    public class Violao {
+    
+    }
+}
