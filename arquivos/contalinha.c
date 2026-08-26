b41e2abd65c2d4b3b23d0a99afc6ce1e8678da40
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-08-26 17:04:29 -0300

    Add files via upload

---
+++ arquivos/contalinha.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// stdout, stdin, stderr
+
+int main(int argc, char** argv)
+{
+	FILE *entrada;
+	int c, count;
+
+	if(argc != 2)
+	{
+		fprintf(stderr,"Erro na chamada do comando.\n");
+		fprintf(stderr,"Uso: %s [ARQUIVO ORIGEM].\n", argv[0]);
+		return 1;
+	}
+
+	entrada = fopen(argv[1],"rb");
+	if(!entrada)
+	{
+		fprintf(stderr,"Arquivo %s não pode ser aberto para leitura\n", argv[1]);
+		return 1;
+	}
+
+	c = fgetc(entrada);
+	while(c != EOF)
+	{
+		if (c=='\n')
+			count++;
+		c = fgetc(entrada);
+	}
+
+	printf("quantidade de linhas: %d", count);
+
+	fclose(entrada);
+	return 0;
+}
+
