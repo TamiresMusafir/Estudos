68e8ec4e7409118a2ce24996b95916775b6dcafe
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-09-14 10:12:16 -0300

    Update intercalacep.c

--- arquivos/intercala/exercicios/intercalacep.c
+++ arquivos/intercala/exercicios/intercalacep.c
@@ -1,89 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
- 
-#define PARTES 8;
+
+#define PARTES 8
 
 typedef struct _Endereco Endereco;
- 
-struct _Endereco{
-    char logradouro[72];
-    char bairro[72];
-    char cidade[72];
-    char uf[72];
-    char sigla[2];
-    char cep[8];
-    char lixo[2];
+
+struct _Endereco
+{
+ char logradouro[72];
+ char bairro[72];
+ char cidade[72];
+ char uf[72];
+ char sigla[2];
+ char cep[8];
+ char lixo[2];
 };
- 
+
 int compara(const void *e1, const void *e2){
-    return strncmp(((Endereco*)e1)->cep,((Endereco*)e2)->cep,8);
+ return strncmp(((Endereco*)e1)->cep,((Endereco*)e2)->cep,8);
 }
 
-int IntercalaArquivos(const void *e1,const void *e2, ){
-    while(!feof(a) && !feof(b))
-    {
-        if(compara(&ea,&eb)<0) // ea < eb
-        {
+int IntercalaArquivos(char e1[],char e2[], char s[]){
+    Endereco ea, eb;
+    FILE *a = fopen(e1, "rb");
+    FILE *b = fopen(e2, "rb");
+    FILE *saida = fopen(s, "wb");  
+
+    if(!a || !b || !saida){
+        fprintf(stderr, "arquivo nao pode ser aberto.\n");
+        return 1;  
+    }
+
+    fread(&ea,sizeof(Endereco),1,a);
+    fread(&eb,sizeof(Endereco),1,b);
+
+    while(!feof(a) && !feof(b)){
+        if(compara(&ea,&eb)<0){ // ea < eb
             fwrite(&ea,sizeof(Endereco),1,saida);
             fread(&ea,sizeof(Endereco),1,a);
-        }
-        else // ea >= eb
-        {
+        }else{ // ea >= eb
             fwrite(&eb,sizeof(Endereco),1,saida);
             fread(&eb,sizeof(Endereco),1,b);
         }
     }
- 
-    while(!feof(a))
-    {
+
+    while(!feof(a)){
         fwrite(&ea,sizeof(Endereco),1,saida);
-        fread(&ea,sizeof(Endereco),1,a);        
+        fread(&ea,sizeof(Endereco),1,a); 
     }
-   
-    while(!feof(b))
-    {
+
+    while(!feof(b)){
         fwrite(&eb,sizeof(Endereco),1,saida);
-        fread(&eb,sizeof(Endereco),1,b);        
+        fread(&eb,sizeof(Endereco),1,b); 
     }
- 
+
     fclose(a);
     fclose(b);
     fclose(saida);
 }
-int main(int argc, char** argv)
-{
-    char nome[80] = {0};
-    Endereco *e;
-    FILE *f;
-    f = fopen("cep.dat", "rb");
-    fseek(f, 0, SEEK_END);
-    int tamnahoArquivo = ftell(f);
-    int qtdTotal = tamnahoArquivo/sizeof(Endereco);
-    int qtdPartes = qtdTotal / PARTES;
-    int resto = qtdTotal%PARTES;
- 
-    rewind(f);
-    for (int i = 0; i < PARTES; i++)
-    {
-        int qtdLido = fread(&e,sizeof(Endereco),qtdPartes+(i<resto?1:0),f);
-        sprintf(nome, "b%d.dat", i);
-        FILE *f = foopen(nome, "wb");
-        qsort(nome,qtdLido/sizeof(Endereco),sizeof(Endereco),compara);
+
+int main(int argc, char** argv){
+ char nome[80] = {0};
+ FILE *f = fopen("cep.dat", "rb");
+
+    if(!f){
+        fprintf(stderr, "Erro ao abrir cep.dat\n");
+        return 1;
     }
-     
-   
- 
-    //Endereco ea, eb;
- 
-    //a = fopen("cep_a.dat","rb");
-    //b = fopen("cep_b.dat","rb");
-    //saida = fopen("saida.dat","wb");
- 
-    //fread(&ea,sizeof(Endereco),1,a);
-    //fread(&eb,sizeof(Endereco),1,b);
- 
-    // eof -> End Of File
-  
+
+ fseek(f, 0, SEEK_END);
+ int tamnahoArquivo = ftell(f);
+ int qtdTotal = tamnahoArquivo/sizeof(Endereco);
+ int qtdPartes = qtdTotal / PARTES;
+ int resto = qtdTotal%PARTES;
+ rewind(f);
+
+ Endereco e = (Endereco) malloc((qtdPartes + 1) * sizeof(Endereco));
+
+ for (int i = 0; i < PARTES; i++){
+    int quantidade = qtdPartes+(i<resto?1:0);
+    int qtdLido = fread(e, sizeof(Endereco), quantidade ,f);
+
+    sprintf(nome, "b%d.dat", i);
+
+    FILE *out = fopen(nome, "wb");
+    qsort(e, qtdLido, sizeof(Endereco), compara);
+    
+    fwrite(e, sizeof(Endereco), qtdLido, out);
+    fclose(out);
+ }
+ fclose(f);
+ free(e);
+
+ char a[80] = {0}, b[80] = {0}, s[80] = {0};
+ int intercalacoes = PARTES-1;
+ for(int j = 0;j < intercalacoes;j++){
+    sprintf(a, "b%d.dat", j*2);
+    sprintf(b, "b%d.dat", j*2+1);
+
+    if( j == intercalacoes - 1){
+        strcpy(s, "cep_ordenado_intercalado.dat");
+    }else{
+        sprintf(s, "b%d.dat", (PARTES)+j);
+    }
+        IntercalaArquivos(a, b, s);
+ }
+    return 0;
 }
