d59c5cc752ed6ff7564c2d1b4278a3a8711e9471
Musafir <119901529+TamiresMusafir@users.noreply.github.com>
2026-09-22 16:38:21 +0000

    exercicio feito

---
+++ arquivos/provasp1/2025.2/intercala.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+struct COVID{
+    int dia;
+    int novos_casos;
+    int obitos;
+};
+
+int compara(const void *a, const void *b){
+    return(((COVID *)a)->dia - ((COVID*)b)->dia);
+}
+
+void intercala(char *arqA, char *arqb, char *arqSaida){
+    FILE *a, *b, *saida;
+    COVID ea, eb, ec;
+
+    a = fopen(arqA, "rb");
+    b = fopen(arqSaida, "rb");
+    saida = fopen(arqSaida, "wb");
+
+    if(!a || !b || !saida)
+        fprintf(stderr, "Não foi possível abrir o arquivo.");
+
+    fread(&ea, sizeof(COVID), 1, a);
+    fread(&eb, sizeof(COVID), 1, b);
+
+    while(!feof(a) && !feof(b)){
+        if(compara(&ea, &eb) < 0){
+            fwrite(&ea, sizeof(COVID), 1, saida);
+            fread(&ea, sizeof(COVID), 1, a);
+        }else if(compara(&ea, &eb) == 0){
+            ec.dia = ea.dia;
+            ec.novos_casos = ea.novos_casos + eb.novos_casos;
+            ec.obitos = ea.obitos + eb.obitos;
+            fwrite(&ec, sizeof(COVID), 1, saida);
+            fread(&ea, sizeof(COVID), 1, a);
+            fread(&eb, sizeof(COVID), 1, b);
+        }else{
+            fwrite(&eb, sizeof(COVID), 1, saida);
+            fread(&eb, sizeof(COVID), 1, b);
+        }
+    }
+
+    while(!feof(a)){
+        fwrite(&ea, sizeof(COVID), 1, saida);
+        fread(&ea, sizeof(COVID), 1, a);
+    }
+
+    while(!feof(b)){
+        fwrite(&eb, sizeof(COVID), 1, saida);
+        fread(&eb, sizeof(COVID), 1, b);
+    }
+
+    fclose(a);
+    fclose(b);
+    fclose(saida);
+}
+
+int main(){
+    
+}
---
+++ arquivos/provasp1/2025.2/lacunaDias.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct COVID{
+    int dia;
+    int novos_casos;
+    int obitos;
+};
+
+int main(){
+    int count = 0;
+
+    FILE *f = fopen("casos.dat", "rb");
+
+    if(!f){
+        fprintf(stderr, "Não foi possível abrir o arquivo %s.\n", "covid.dat");
+        return 1;
+    }
+
+    COVID c;
+
+    while(fread(&c, sizeof(COVID), 1, f) == 1){
+        if(c.dia == count)
+            count++;
+        else{
+            printf("Lacuna encontrada no dia %d", count);
+            count++;
+        }
+    }
+}
